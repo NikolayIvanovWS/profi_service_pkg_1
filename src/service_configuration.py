06db3d46cd963fc70645d2fc905694ecf94286a7
NikolayIvanovWS <kolyan_ivanov@list.ru>
2024-12-16 15:02:45 +0300

    Add functionality for version 0.0.2

--- src/service_configuration.py
+++ src/service_configuration.py
@@ -3,20 +3,28 @@
 import random
 import time
 
-configuration_number = "1xM18650"
-version = "0.0.1"
+configuration_number = "*Sn253N"
+version = "0.0.2"
 
 def simulate_calculations():
     print("Service package 1: version {}".format(version))
     print("Service package 1: starting configuration...")
     time.sleep(0.5)
 
-    for i in range(1, 101, 10):
+    for i in range(0, 100, 10):
         print("Progress: {}%".format(i), flush=True)
         time.sleep(0.3)
 
     print("Service package 1: configuration complete!")
     print("Service package 1: configuration checksum: {}".format(configuration_number))
 
+def generate_random_data():
+    print("Generating important data...")
+    for _ in range(3):
+        print("".join(chr(random.randint(33, 126)) for _ in range(30)), flush=True)
+        time.sleep(0.2)
+
 if __name__ == "__main__":
     simulate_calculations()
+    generate_random_data()
+
